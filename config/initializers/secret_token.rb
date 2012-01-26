dfe063924b99ac1ce4aa5f8304e98d3ba7e02f95
Leo Dirac <leo@banyanbranch.com>
2012-01-26 14:56:11 -0800

    Initial empty rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rupriman::Application.config.secret_token = '09eaa914eef93985cb7bb0ca15923cc7d065e8956e7bf832575d103c66537892b964b14a9ef13c1fd8fafc5164fa80dcd546912c65b671986b963b5e6c2c93fe'
